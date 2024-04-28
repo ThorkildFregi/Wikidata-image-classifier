f5dff461a2ba960d7c420f6e837c7908c7629146
Gabriel Bermes--Poupeau <gabrielbermesp@gmail.com>
2024-04-28 22:30:56 +0200

    Update start_conda.bat

--- start_conda.bat
+++ start_conda.bat
@@ -2,6 +2,6 @@ call "\Path\To\Anaconda3\Scripts\activate.bat"
 
 call conda activate imgClassifierTrainerWikidata
 
-cd Path\To\(main.py's folder parent)
+cd Path\To\ # (main.py's folder parent)
 
 flask --app main.py --debug run
