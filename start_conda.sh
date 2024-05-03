c509ec01a97950551d5735dc0fd97c2214716f60
Gabriel Bermes--Poupeau <gabrielbermesp@gmail.com>
2024-05-03 22:41:22 +0200

    Delete start_conda.sh

--- start_conda.sh
+++
@@ -1,7 +0,0 @@
-!source "\Path\To\Anaconda3\Scripts\activate.bat"
-
-!source conda activate imgClassifierTrainerWikidata
-
-!cd Path\To\ # (main.py's folder parent)
-
-!flask --app main.py --debug run
